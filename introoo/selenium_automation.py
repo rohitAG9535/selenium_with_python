6048d5c0349bd7beb94845e26ca7edf3d8651168
Rohit <Rohit@SMTC-R-0159>
2023-03-30 16:30:14 +0530

    selenium

--- introoo/selenium_automation.py
+++ introoo/selenium_automation.py
@@ -1,1 +1,19 @@
 print("good evening")
+
+
+import time
+from selenium import webdriver
+
+driver=webdriver.chrome()
+
+driver.get("https://www.ecoders.com")
+
+time.sleep(5)
+
+print(driver.title)
+
+print(driver.current_url)
+
+print(driver.page_source)
+
+driver.quit()
